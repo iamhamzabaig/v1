5b4a28393a9cfbd4edb7eae884e37fe11e4e7b9a
Hamza Baig <hamzamunawar345@gmail.com>
2024-05-15 00:05:26 +0500

    abc

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+	const menu = document.querySelector(".menu-links");
+	const icon = document.querySelector(".hamburger-icon");
+	menu.classList.toggle("open");
+	icon.classList.toggle("open");
 }
+
+window.addEventListener("load", function () {
+	setTimeout(function () {
+		document.querySelector(".loader-wrapper").style.display = "none";
+		document.querySelector(".content").classList.remove("hidden");
+	}, 2200);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	// Add 'show' class to nav-links after a delay
+	setTimeout(function () {
+		document.querySelector(".nav-links").classList.add("show");
+	}, 2500); // Adjust the delay time as needed
+});
